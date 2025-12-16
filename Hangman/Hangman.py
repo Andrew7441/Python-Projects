a383c22b5c10818e88606e6de39b63b9b3018fdc
Andrew <104359161+Andrew7441@users.noreply.github.com>
2025-12-16 14:13:00 +0200

    Add Hangman project

---
+++ Hangman/Hangman.py
@@ -0,0 +1,45 @@
+import random
+from hangmanart import stages
+from hangmanart import logo
+from hangmanwords import word_list
+
+print(logo)
+
+chosen_word = random.choice(word_list)
+print(chosen_word) # testing
+
+placeholder = ""
+for i in chosen_word:
+    placeholder += "_"
+print(placeholder)
+
+game_over = False
+correct_letters = []
+lives = 6
+
+while not game_over:
+    guess = input("Guess a letter: ").lower()
+
+    display = ""
+
+    for letter in chosen_word:
+        if guess == letter:
+            display += letter
+            correct_letters.append(letter)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
+
+    print(display)
+    
+    if guess not in chosen_word:
+        print(stages[lives])
+        lives-=1
+        if lives < 0:
+            print("You Lose")
+            game_over = True
+
+    if "_" not in display:
+        print("You Win!")
+        game_over = True
